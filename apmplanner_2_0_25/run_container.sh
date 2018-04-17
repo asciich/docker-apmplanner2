0867619e02c15e85e2741fcb78759cbcd3f32435
asciich <reto.hasler@asciich.ch>
2018-04-17 18:49:14 +0200

    'general'

--- apmplanner_2_0_25/run_container.sh
+++ apmplanner_2_0_25/run_container.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-echo ""
 echo "Start container"
 VOLUMES="-v $(git rev-parse --show-toplevel):/git-repo/"
 VOLUMES="${VOLUMES} -v /tmp/.X11-unix:/tmp/.X11-unix"
 VOLUMES="${VOLUMES} -v ${XAUTHORITY}:${XAUTHORITY}"
 ENVIRONMENT="-e DISPLAY=${DISPLAY}"
 ENVIRONMENT="${ENVIRONMENT} -e XAUTHORITY=${XAUTHORITY}"
-CONTAINER=asciich/ubuntu_apmplanner:2.0.25
+CONTAINER=asciich/apmplanner2:2.0.25
 
-docker run --rm --net=host --privileged ${VOLUMES} ${ENVIRONMENT}-it ${CONTAINER} /start_apmplanner2.sh
+docker run --rm --net=host --privileged ${VOLUMES} ${ENVIRONMENT} -it ${CONTAINER} /start_apmplanner2.sh
